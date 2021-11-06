2a96ac9343ed71c7502c636abdbe0adec6d55196
Jang Woong Geun <janggomgeun@gmail.com>
2021-11-06 21:03:16 +0900

    Reformat

--- src/popup.js
+++ src/popup.js
@@ -1,88 +1,78 @@
-'use strict';
+"use strict";
 
-import { ChromeTabs } from './libs/chrome-api/chrome-tabs';
-import { ChromeRuntime } from './libs/chrome-api/chrome-runtime';
-import './popup.css';
-import { BACKGROUND_API } from './background';
+import { ChromeTabs } from "./libs/chrome-api/chrome-tabs";
+import { ChromeRuntime } from "./libs/chrome-api/chrome-runtime";
+import "./popup.css";
+import { BACKGROUND_API } from "./background";
 
 class Popup {
   async init() {
-    this.chromeRuntime = new ChromeRuntime()
-    this.chromeTabs = new ChromeTabs()
-    this.appDOM = document.getElementById('app')
-    this.clipContentsButtonDOM = document.getElementById('clipContentsBtn')
-    this.bindButtonDOM = document.getElementById('bindBtn')
-    this.currentBinderDOM = document.getElementById('current-binder')
-    this.bindersDOM = document.getElementById('binders')
-    this.bind()
+    this.chromeRuntime = new ChromeRuntime();
+    this.chromeTabs = new ChromeTabs();
+    this.appDOM = document.getElementById("app");
+    this.clipContentsButtonDOM = document.getElementById("clipContentsBtn");
+    this.bindButtonDOM = document.getElementById("bindBtn");
+    this.currentBinderDOM = document.getElementById("current-binder");
+    this.bindersDOM = document.getElementById("binders");
+    this.bind();
   }
 
   bind() {
-    const self = this
-    this.clipContentsButtonDOM.addEventListener('click', function(e) {
-      self.onClipContentsButtonClicked()
-    })
+    const self = this;
+    this.clipContentsButtonDOM.addEventListener("click", function (e) {
+      self.onClipContentsButtonClicked();
+    });
 
-    this.bindButtonDOM.addEventListener('click', function(e) {
-      self.onBindButtonClicked()
-    })
+    this.bindButtonDOM.addEventListener("click", function (e) {
+      self.onBindButtonClicked();
+    });
   }
 
   async onClipContentsButtonClicked() {
-    console.log('onClipContentsButtonClicked')
+    console.log("onClipContentsButtonClicked");
     const tabs = await this.chromeTabs.query({
       active: true,
       currentWindow: true,
-      highlighted: true
-    })
+      highlighted: true,
+    });
 
     if (!tabs.length) {
-      throw new Error('An active tab on the current window is not found')
+      throw new Error("An active tab on the current window is not found");
     }
 
-    const activeCurrentTab = tabs[0]
+    const activeCurrentTab = tabs[0];
     console.log(activeCurrentTab);
     try {
-      const response = await this.chromeRuntime.sendMessage(
-        {
-          type: `${BACKGROUND_API.namespace}.${BACKGROUND_API.apis.CLIP_CONTENTS}`,
-          payload: {
-            url: activeCurrentTab.url,
-          }
-        }
-      )
-      console.log('response', response);
+      const response = await this.chromeRuntime.sendMessage({
+        type: `${BACKGROUND_API.namespace}.${BACKGROUND_API.apis.CLIP_CONTENTS}`,
+        payload: {
+          url: activeCurrentTab.url,
+        },
+      });
+      console.log("response", response);
     } catch (error) {
       console.log(error);
     }
   }
-  
+
   onBindButtonClicked() {
-    console.log('onBindButtonClicked')
+    console.log("onBindButtonClicked");
   }
 
-  onClearBinderButtonClicked() {
+  onClearBinderButtonClicked() {}
 
-  }
+  onBinderFocusChanged() {}
 
-  onBinderFocusChanged() {
+  onClearFocusButtonClicked() {}
 
-  }
-
-  onClearFocusButtonClicked() {
-
-  }
-
-  onBinderDeleted() {
-
-  }
+  onBinderDeleted() {}
 }
 
-(function(){
+(function () {
   function initEnvironment() {
-    const popup = new Popup()
-    document.addEventListener('DOMContentLoaded', popup.init())
+    const popup = new Popup();
+    document.addEventListener("DOMContentLoaded", popup.init());
   }
-  
+
   initEnvironment();
-})()
+})();
